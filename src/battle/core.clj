72b174ff5309c788712822ce51de5ff8d977bd20
Juhana Laurinharju <fingerzam@gmail.com>
2012-03-12 19:04:47 +0200

    initial commit

---
+++ src/battle/core.clj
@@ -0,0 +1,1 @@
+(ns battle.core)
---
+++ test/battle/test/core.clj
@@ -0,0 +1,6 @@
+(ns battle.test.core
+  (:use [battle.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
